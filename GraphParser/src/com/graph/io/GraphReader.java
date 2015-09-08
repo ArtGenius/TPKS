13ce13f95a7d8bc7b8a7d6e044757b98a9cbb401
Roman <Roman@romario>
2015-09-08 22:07:41 +0300

    Добавлены ошибки(GraphReader) + новый граф(10 вершин, 17 дуг).

--- GraphParser/src/com/graph/io/GraphReader.java
+++ GraphParser/src/com/graph/io/GraphReader.java
@@ -29,6 +29,9 @@ public class GraphReader {
 			int rows;
 			int cols;
 			cols = Integer.parseInt(in.readLine());
+			if(cols>16){
+				System.out.println("Входные данные не соответствуют заданным ограничениям(количество вершин не больше 16).");
+			}
 			rows = Integer.parseInt(in.readLine());
 			matrix = new IncidenceMatrix(cols, rows);
 			String temp;
@@ -38,8 +41,10 @@ public class GraphReader {
 				byte row[] = new byte[(cols%4==0)? (cols/4):(cols/4)+1];
 				int j = 0;
 				byte trow = 0;
+				int one=0;
+				int mone=0;
 				for (int i = 0; i < cols; i++) {
-					if (i % 4 == 0 && i!=0) {
+				if (i % 4 == 0 && i!=0) {
 						++j;
 					}
 					switch (tmp[i]) {
@@ -48,15 +53,23 @@ public class GraphReader {
 						break;
 					case "1":
 						trow = 1;
+						++one;
 						break;
 					case "-1":
 						trow = 2;
+						++mone;
 						break;
 					default:
 						break;
 					}
+					if(one>1||mone>1){
+						System.out.println("Ребро не может соединять более двух вершин.");
+					}
 					trow = (byte) (trow << (6 - (i % 4) * 2));
 					row[j]=(byte)(row[j]|trow);
+				}
+				if(one==0||mone==0){
+					System.out.println("Ребро не имеет начала или конца.");
 				}
 				matrix.setRow(row);
 				
--- GraphParser/src/com/graph/io/GraphWriter.java
+++ GraphParser/src/com/graph/io/GraphWriter.java
@@ -12,16 +12,21 @@ public class GraphWriter {
 	private File outputFile;
 	public GraphWriter(String outputFileName) {
 		outputFile=new File(outputFileName);
-		try(BufferedWriter out=new BufferedWriter(new FileWriter(outputFile))){
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		
 	}
 
 	public void write(ArcList list) {
 		// TODO разработать функцию записи списка дуг в файл
+		
+try(BufferedWriter out=new BufferedWriter(new FileWriter(outputFile))){
+	         for(int i=0; i<list.getSize()/2;i++){
+			 out.write(i+")"+"  "+list.get()+"  "+list.get());
+			 out.newLine();
+	         }
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
